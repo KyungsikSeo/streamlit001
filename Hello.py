c2efe789ac353db2777276b34cea9374d23dc278
KyungsikSeo <sks8051@gmail.com>
2024-10-20 19:36:34 +0900

    update

--- Hello.py
+++ Hello.py
@@ -1,28 +1,27 @@
 import streamlit as st
+from streamlit_folium import folium_static
+import folium
+from folium.plugins import MarkerCluster
+import pandas as pd
 
-st.set_page_config(
-    page_title="Hello",
-    page_icon="👋",
-)
+st.title("팅이네 미동여지도!")
 
-st.write("# Tingi's Korean Restaurant Map! 👋")
+df = pd.read_csv("maps.csv", encoding='ansi')
 
-st.sidebar.success("Select a demo above.")
+st.dataframe(df, height=200)
 
-st.markdown(
-    """
-    Streamlit is an open-source app framework built specifically for
-    Machine Learning and Data Science projects.
-    **👈 Select a demo from the sidebar** to see some examples
-    of what Streamlit can do!
-    ### Want to learn more?
-    - Check out [streamlit.io](https://streamlit.io)
-    - Jump into our [documentation](https://docs.streamlit.io)
-    - Ask a question in our [community
-        forums](https://discuss.streamlit.io)
-    ### See more complex demos
-    - Use a neural net to [analyze the Udacity Self-driving Car Image
-        Dataset](https://github.com/streamlit/demo-self-driving)
-    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-"""
-)
+df[["lat","lon"]] = df[["위도","경도"]]
+
+#m = folium.Map(location=[36.350412, 127.384548], zoom_start=13)
+#36.229796, 127.763104
+m = folium.Map(location=[36.229796, 127.763104], zoom_start=7)
+
+marker_cluster = MarkerCluster().add_to(m)
+
+for idx, row in df.iterrows():
+    folium.Marker(
+        location=[row["lat"], row["lon"]],
+        popup=row["업소명"],
+    ).add_to(marker_cluster)
+
+folium_static(m)
